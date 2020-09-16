cf1ff6386b6bd626ff77e7e150d642843f4ffc56
mzubrzyc <52781330+mzubrzyc@users.noreply.github.com>
2020-09-16 11:15:49 +0200

    git test - 1

---
+++ src/com/git/test/Main.java
@@ -0,0 +1,11 @@
+package com.git.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+	
+	System.out.println("Git test");
+
+    }
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module com.git.test {
+}
